6a6dbcb6dbe1a56592ef2514a20e011b77b11b64
Christian Drengenberg <ChristianDrengenberg@googlemail.com>
2017-02-21 12:01:29 +0100

    Added documentation.

--- index.js
+++ index.js
@@ -6,20 +6,84 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const extend = require('extend');
 const logger = require('winston');
+const Promise = require('bluebird');
 
-// -- Enums -> ----------------------------------------------------------------------------
+/**
+ * Module for general and common initialization of the OpusCapita Business Network
+ * web server module providing default behavior for RESTful services.
+ * @module ocbesbn-web-init
+ * @requires express
+ * @requires webpack
+ * @requires body-parser
+ * @requires cookie-parser
+ * @requires helmet
+ * @requires morgan
+ * @requires extend
+ * @requires winston
+ * @requires bluebird
+ */
+
+/**
+ * Static object containing enumeration objects providing server behavior configuration.
+ */
 module.exports.Server = {
+    /**
+     * Enumeration made to control a server's environment behavior.
+     * @enum {number}
+     */
     Mode : { None : 0, Productive : 1, Dev : 2, Debug : 3 },
+
+    /**
+     * List of flags for security configuration.
+     * @enum {number}
+     */
     Security : { All : 0, AllowCrossOrigin : 1 } // Bitword
 }
 
+/**
+ * Static object containing enumeration objects providing behavior for Morgan logging.
+ */
 module.exports.Morgan = {
+    /**
+     * Enumeration for preselected Morgan logging styles.
+     * @enum {string}
+     */
     Format : { Combined : 'combined', Common : 'common', Dev : 'dev', Short : 'short', Tiny : 'tiny', Custom : (callback) => callback },
+
+    /**
+     * Enumeration provising some standard Morgan output streams.
+     * @enum {Stream}
+     */
     OutputStream : { StdOut : process.stdout, StdErr : process.stderr, Console : { write : console.log }, Custom : (callback) => { write : callback } }
 }
-// -- <- Enums  ---------------------------------------------------------------------------
 
-// -- Config -> ----------------------------------------------------------------------------
+/**
+ * Static object representing a default configuration set for ocbesbn-web-init.
+ *
+ * @property {object} server - All server web server related settings.
+ * @property {Server.Mode} server.mode - Environment [mode]{@link module:ocbesbn-web-init.Server.Mode} to run in.
+ * @property {Server.Security} server.security - Bitword for applying multiple [security]{@link module:ocbesbn-web-init.Server.Security} presets.
+ * @property {array} server.crossOrigins - A list of allowed cross-origins source domains.
+ * @property {number} server.maxBodySize - Maximum size in bytes of a http-reuqest-body.
+ * @property {string} server.staticFilePath - Path for all static files delivered by the web server.
+ * @property {string} server.hostname - The local host name or ip-address to bind the server to.
+ * @property {number} server.port - The local tcp port for the server to listen on.
+ * @property {object} server.events - Object providing access points to some life cycle events.
+ * @property {function} server.events.onStart - Start event fired when successfully listens for incoming connections.
+ * @property {function} server.events.onEnd - End event fired whenever the server shuts down.
+ * @property {function} server.events.onRequest - Fired every time, a request comes in.
+ * @property {function} server.events.onError - Fired if the server could not start.
+ * @property {object} server.webpack - Webpack configuration.
+ * @property {boolean} server.webpack.useWebpack - Controls whenever the server should use Webpack.
+ * @property {boolean} server.webpack.configFilePath - Webpack configuration file path.
+ * @property {object} routes - Basic routing configuration for the RESTful web server.
+ * @property {boolean} routes.addRoutes - Controls whenever routes should be added to be accessible via http.
+ * @property {array} routes.modelPaths - List of modules to load in order to register wev server routes.
+ * @property {object} routes.dbInstance - Databse object to be passed to every module registering routes.
+ * @property {object} morgan - Basic morgan logging configuration.
+ * @property {Morgan.Format} morgan.format - Morgan [log format]{@link module:ocbesbn-web-init.Morgan.Format}.
+ * @property {Morgan.OutputStream} morgan.stream - Morgan output [stream]{@link module:ocbesbn-web-init.Morgan.OutputStream}.
+ */
 module.exports.DefaultConfig = {
     server : {
         mode : process.env.NODE_ENV === 'development' ? this.Server.Mode.Dev : this.Server.Mode.Productive,
@@ -50,8 +114,14 @@ module.exports.DefaultConfig = {
         stream : this.Morgan.OutputStream.Console
     }
 }
-// -- <- Config ----------------------------------------------------------------------------
 
+/**
+ * Initializes an instance of ocbesbn-web-init by passing a set of optional configuration values.
+ * This method also registeres the process signals SIGTERM and SIGINT and runs a clean shutdown on the server.
+ *
+ * @param {object} config - Optional configuration object extending {@link module:ocbesbn-web-init.DefaultConfig}.
+ * @returns {Promise} [Promise]{@link http://bluebirdjs.com/docs/api-reference.html} containing an [Express]{@link https://github.com/expressjs/express} instance.
+ */
 module.exports.init = function(config) {
 
     this.config = config = extend(true, { }, this.DefaultConfig, config);
@@ -111,8 +181,10 @@ module.exports.init = function(config) {
     {
         logger.log('debug', 'Adding routes...');
 
-        ((paths, db, config) => Array.isArray(paths) ? paths.forEach(path => require(path).init(app, db, config))
+        var inits = ((paths, db, config) => Array.isArray(paths) ? paths.map(path => require(path).init(app, db, config))
             : [ require(paths).init(app, db, config) ])(config.routes.modulePaths, config.routes.dbInstance, config.routes);
+
+        Promise.all(inits).reflect();
     }
 
     var self = this;
@@ -128,12 +200,14 @@ module.exports.init = function(config) {
 
     logger.log('info', 'Server started.');
 
-    return app;
+    return Promise.resolve(app);
 }
 
+/**
+ * Ends an initialized instance of ocbesbn-web-init.
+ */
 module.exports.end = function()
 {
-
     var self = this;
 
     if(this.server)
